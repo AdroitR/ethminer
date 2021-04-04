a5a7c0a232a6d54a3e33688e3246455ebe6a7420
Kien R <20687796+AdroitR@users.noreply.github.com>
2021-04-03 23:21:25 -0500

    Add files via upload

---
+++ ethminer/CMakeFiles/ethminer.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bizon/miners/ethermine/ethminer/ethminer/main.cpp" "/home/bizon/miners/ethermine/ethminer/build/ethminer/CMakeFiles/ethminer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "API_CORE"
+  "BIN_KERN"
+  "ETH_ETHASHCUDA"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ethminer/.."
+  "/usr/local/cuda-11.1/include"
+  "."
+  "/home/bizon/.hunter/_Base/4b894e1/51efd6e/cb395ce/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bizon/miners/ethermine/ethminer/build/libethcore/CMakeFiles/ethcore.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/libdevcore/CMakeFiles/devcore.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/CMakeFiles/ethminer-buildinfo.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/libapicore/CMakeFiles/apicore.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/libethash-cuda/CMakeFiles/ethash-cuda.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/libhwmon/CMakeFiles/hwmon.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ethminer/buildinfo.ps1
@@ -0,0 +1,5 @@
+$env:project_name="ethminer"
+$env:project_version="0.19.0-11+commit.d5f3a972.dirty"
+$env:project_version_is_prerelease="true"
+$env:system_name='linux'
+$env:system_processor='x86_64'
