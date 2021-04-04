df85a08b38475d88e37fbde446c0e99f4910f19a
Kien R <20687796+AdroitR@users.noreply.github.com>
2021-04-03 23:22:38 -0500

    Add files via upload

---
+++ libpoolprotocols/CMakeFiles/poolprotocols.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bizon/miners/ethermine/ethminer/libpoolprotocols/PoolManager.cpp" "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/PoolManager.cpp.o"
+  "/home/bizon/miners/ethermine/ethminer/libpoolprotocols/PoolURI.cpp" "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/PoolURI.cpp.o"
+  "/home/bizon/miners/ethermine/ethminer/libpoolprotocols/getwork/EthGetworkClient.cpp" "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/getwork/EthGetworkClient.cpp.o"
+  "/home/bizon/miners/ethermine/ethminer/libpoolprotocols/stratum/EthStratumClient.cpp" "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/stratum/EthStratumClient.cpp.o"
+  "/home/bizon/miners/ethermine/ethminer/libpoolprotocols/testing/SimulateClient.cpp" "/home/bizon/miners/ethermine/ethminer/build/libpoolprotocols/CMakeFiles/poolprotocols.dir/testing/SimulateClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "API_CORE"
+  "BIN_KERN"
+  "ETH_ETHASHCUDA"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libpoolprotocols/.."
+  "."
+  "/home/bizon/.hunter/_Base/4b894e1/51efd6e/cb395ce/Install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bizon/miners/ethermine/ethminer/build/libdevcore/CMakeFiles/devcore.dir/DependInfo.cmake"
+  "/home/bizon/miners/ethermine/ethminer/build/CMakeFiles/ethminer-buildinfo.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
